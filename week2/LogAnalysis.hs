c4ac3270470f7d34a8ba99e6882c1ca81ee89a04
kendricktan <kendricktan0814@gmail.com>
2017-01-12 01:05:42 +1000

    week2

---
+++ week2/LogAnalysis.hs
@@ -0,0 +1,34 @@
+{-# OPTIONS_GHC -Wall #-}
+module LogAnalysis where
+
+import Data.List
+import Data.Char
+import Log
+
+filterNonNumbers :: [String] -> [String]
+filterNonNumbers = filter (all isDigit)
+
+filterNumbers :: [String] -> [String]
+filterNumbers = filter (all isLetter)
+
+parseMessageType :: [String] -> Maybe MessageType
+parseMessageType s
+  | nc == 'e' = Just (Error x)
+  | nc == 'i' = Just Info
+  | nc == 'w' = Just Warning
+  | otherwise = Nothing
+  where nc = (toLower . head . head) s
+        x  = read (s !! 1) :: Int
+
+parseLogMessage :: Maybe MessageType -> [String] -> LogMessage
+parseLogMessage Nothing  s = Unknown (intercalate " " s)
+parseLogMessage (Just a) s = LogMessage a (read ts :: Int) (intercalate " " rs)
+  where nn = filterNonNumbers s -- Non Numbers
+        ts = (head . reverse) nn   -- Time Stamp
+        rs = (drop 1 $ filterNumbers s)
+        
+
+parseMessage :: String -> LogMessage
+parseMessage s = parseLogMessage pmt wl
+  where wl = words s
+        pmt = parseMessageType wl
