cb48120c5ad9128ec3927efd384e2aa72bb7fc2c
Kendrick Tan <kendricktan0814@gmail.com>
2017-01-12 17:32:42 +1000

    finished week 2

--- week2/LogAnalysis.hs
+++ week2/LogAnalysis.hs
@@ -5,11 +5,10 @@ import Data.List
 import Data.Char
 import Log
 
+-- Exercise 1 --
+-- Convert raw inputs to LogMessage type
 filterNonNumbers :: [String] -> [String]
 filterNonNumbers = filter (all isDigit)
-
-filterNumbers :: [String] -> [String]
-filterNumbers = filter (all isLetter)
 
 parseMessageType :: [String] -> Maybe MessageType
 parseMessageType s
@@ -25,8 +24,9 @@ parseLogMessage Nothing  s = Unknown (unwords s)
 parseLogMessage (Just a) s = LogMessage a (read ts :: Int) (unwords rs)
   where nn = filterNonNumbers s -- Non Numbers
         ts = (head . reverse) nn   -- Time Stamp
-        rs = (drop 1 $ filterNumbers s)
-        
+        rs = if (toLower . head . head) s == 'e'
+             then drop 3 s
+             else drop 2 s
 
 parseMessage :: String -> LogMessage
 parseMessage s = parseLogMessage pmt wl
@@ -35,3 +35,37 @@ parseMessage s = parseLogMessage pmt wl
 
 parse :: String -> [LogMessage]
 parse s = map parseMessage (lines s)
+
+-- Exercise 2 --
+logMsgTimeStamp :: LogMessage -> Maybe Int
+logMsgTimeStamp (Unknown _)        = Nothing
+logMsgTimeStamp (LogMessage _ t _) = Just t
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert l1 Leaf            = Node Leaf l1 Leaf
+insert l1 (Node m1 l2 m2)
+  | b == a = Node m1 l2 m2
+  | b < a  = Node (LogAnalysis.insert l1 m1) l2 m2
+  | b > a  = Node m1 l2 (LogAnalysis.insert l1 m2)
+  where b = logMsgTimeStamp l1
+        a = logMsgTimeStamp l2
+
+-- Exercise 3 --
+build :: [LogMessage] -> MessageTree
+build = foldr LogAnalysis.insert Leaf
+
+-- Exercise 4 --
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf          = []
+inOrder (Node l lm r) = inOrder l ++ [lm] ++ inOrder r
+
+-- Exercise 5 --
+isLogErrorSevere :: LogMessage -> Bool
+isLogErrorSevere (LogMessage (Error n) _ _) = n >= 50
+isLogErrorSevere _                  = False
+
+logMsgString :: LogMessage -> String
+logMsgString (LogMessage _ _ m) = m
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong l = map logMsgString $ filter isLogErrorSevere l
